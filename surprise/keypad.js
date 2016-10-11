c8cfaad35628b816c5c87bc59e2e9bb206183903
christianjonathancoan <christian.jonathan.coan+portfolio@gmail.com>
2016-10-11 10:26:06 +0100

    Update keypad.js

--- surprise/keypad.js
+++ surprise/keypad.js
@@ -1,31 +1,57 @@
-$(function () {
-	$(".content").click(function () {
+$(function() {
+  var clicked = false;
 
-		var value = $(this).find(".number").text();
+  $(".content").click(function() {
+    if (!clicked) {
+      var value = $(this).find(".number").text();
 
-		if (value !== "<") {
-			$(".numberinput").each(function () {
-				var a = $(this).text();
-				if (!a) {
-					$(this).text(value);
-					$(this).addClass("nocircle");
-					return false;
-				}
-			});
-		} else {
-			$($(".numberinput").get().reverse()).each(function () {
-				var a = $(this).text();
-				if (a) {
-					$(this).text("");
-					$(this).removeClass("nocircle");
-					return false;
-				}
-			});
-		}
-	});
+      if (value === ">") {
+        clicked = true;
+        var surpriseNumber = "";
+        var counter = 0;
+        $(".numberinput")
+          .each(function() {
+            var a = $(this).text();
+            if (a) {
+              surpriseNumber += a;
+              counter += 1;
+            }
+          });
+	      
+	 if(counter === 4) 
+	 	checkNumbers(surpriseNumber);
+	 else
+		 clicked = false;
+      } else if (value !== "<") {
+        $(".numberinput")
+          .each(function() {
+            var a = $(this).text();
+            if (!a) {
+              $(this).text(value);
+              $(this).addClass("nocircle");
+              return false;
+            }
+          });
+      } else {
+        $($(".numberinput").get().reverse())
+          .each(function() {
+            var a = $(this).text();
+            if (a) {
+              $(this).text("");
+              $(this).removeClass("nocircle");
+              return false;
+            }
+          });
+      }
+    }
+  });
 });
 
-function CheckNumbers(value) {	
-	if(value === "blah")
+function checkNumbers(value) {	
+	if(parseInt(value) === 1234)
 		window.location.href = "https://www.milsomhotels.com/maison-talbooth/accommodation/";
+	else {
+		clicked = false;
+          	alert("You have entered an invalid number");
+	}
 }
